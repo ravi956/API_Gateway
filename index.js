bf77ff9d333cb3426d4a5be26b54cdcd79103ca0
Ravi Kumar <ravikumar956078@gmail.com>
2023-03-22 20:28:30 +0530

    Added pm2 package

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const morgan = require('morgan');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const rateLimit = require('express-rate-limit');
 const axios = require('axios');
-const { PORT } = require('./serverConfig');
+// const { PORT } = require('./serverConfig');
+const PORT = 3005;
 
 const app = express();
 
